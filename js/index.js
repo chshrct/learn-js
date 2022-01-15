d8b5efdd0e9a95b23bfbc565c08226dfa7b5df00
Yauheni <eugene.kostroma@gmail.com>
2022-01-16 01:03:26 +0400

    recursion-task3

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-// Задача – написать функцию factorial(n), которая возвращает n!, используя рекурсию.
+// Напишите функцию fib(n) которая возвращает n-е число Фибоначчи.
 
-function factorial(n){
 
-  return (n>1)?n*factorial(n-1):n;
+function fib(n) {
+  return (n<=1)?n:fib(n-1)+fib(n-2);
+
 
 }
 
 
 
-
-alert( factorial(5) ); // 120
+alert(fib(3)); // 2
+alert(fib(7)); // 13
+alert(fib(77)); // 5527939700884757
