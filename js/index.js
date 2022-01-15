867411d3068c729297f6d3b3e1e9536c5be65441
Yauheni <eugene.kostroma@gmail.com>
2022-01-16 00:48:08 +0400

    recursion-task2

--- js/index.js
+++ js/index.js
@@ -1,27 +1,12 @@
-// Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+// Задача – написать функцию factorial(n), которая возвращает n!, используя рекурсию.
 
-// Сделайте три варианта решения:
+function factorial(n){
 
-// С использованием цикла.
-// Через рекурсию, т.к. sumTo(n) = n + sumTo(n-1) for n > 1.
-// С использованием формулы арифметической прогрессии.
+  return (n>1)?n*factorial(n-1):n;
 
-// cycle
-// function sumTo(n) {
-//   let sum =0;
-//   for (i=1;i<=n;i++){
-//     sum+=i;
-//   }
-//   return sum;
-// }
-// recursion
-// function sumTo(n){
-//   return (n>1)?n+sumTo(n-1):n;
-// }
-// progression
-function sumTo(n){
-
-  return ((2+(n-1))/2)*n;
 }
 
-alert( sumTo(100) ); // 5050
+
+
+
+alert( factorial(5) ); // 120
