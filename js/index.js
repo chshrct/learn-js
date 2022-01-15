f12a1b8dc25d62d0e498eec9f7aed18d3b574125
Yauheni <eugene.kostroma@gmail.com>
2022-01-15 22:40:33 +0400

    salaries-myattempt

--- js/index.js
+++ js/index.js
@@ -1,6 +1,47 @@
 'use strict'
 
-function powAb(a,b) {
-  return (b==1)?a:a*powAb(a,b-1)
+let company = {
+  sales: [{
+    name: 'John',
+    salary: 1000
+  }, {
+    name: 'Alice',
+    salary: 600
+  }],
+
+  development: {
+    sites: [{
+      name: 'Peter',
+      salary: 2000
+    }, {
+      name: 'Alex',
+      salary: 1800
+    }],
+
+    internals: [{
+      name: 'Jack',
+      salary: 1300
+    }]
+  }
+};
+
+
+function salaries(params) {
+  let sumSal = 0;
+  if(Array.isArray(params)){
+
+    params.forEach(element => {
+    sumSal += element.salary;
+
+    });
+    return sumSal;
+    
+  }else{
+    for (let key in params){
+      sumSal+=salaries(params[key])
+    }
+    return sumSal;
+  }
 }
-console.log(powAb(2,3));
+
+console.log(salaries(company)); 
