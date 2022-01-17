26f62e56b4c735d989a995a52604a07d1f57de2f
Yauheni <eugene.kostroma@gmail.com>
2022-01-17 23:46:55 +0400

    closure

--- js/index.js
+++ js/index.js
@@ -1,43 +1,12 @@
-// Выведите односвязный список из предыдущего задания Вывод односвязного списка в обратном порядке.
+// Напишите функцию sum, которая работает таким образом: sum(a)(b) = a+b.
 
-// Сделайте два решения: с использованием цикла и через рекурсию.
+// Да, именно таким образом, используя двойные круглые скобки (не опечатка).
 
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null
-      }
-    }
-  }
-};
-//recursion
-// function printListRev(list) {
-  
-// if(list.next){
-//   printListRev(list.next)
-// }
-// console.log(list.value);
 
-// }
-
-function printListRev(list) {
-  let values = [];
-  let qwe = list;
-
-  while (qwe) {
-    values.push(qwe.value);
-    qwe = qwe.next;
-  }
-
-  for (let i = values.length - 1; i >= 0; i--) {
-    console.log( values[i] );
+function sum(a) {
+  return function(b) {
+    return a+b
   }
 }
 
-
-printListRev(list)
+console.log(sum(4)(5))
