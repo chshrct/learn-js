53b04b2c39079dd64aead64bf892ad733ee800b2
Yauheni <eugene.kostroma@gmail.com>
2022-01-18 00:06:00 +0400

    closure-task3

--- js/index.js
+++ js/index.js
@@ -1,27 +1,28 @@
-// У нас есть встроенный метод arr.filter(f) для массивов. Он фильтрует все элементы с помощью функции f. Если она возвращает true, то элемент добавится в возвращаемый массив.
+// У нас есть массив объектов, который нужно отсортировать:
 
-// Сделайте набор «готовых к употреблению» фильтров:
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+// Обычный способ был бы таким:
 
-// inBetween(a, b) – между a и b (включительно).
-// inArray([...]) – находится в данном массиве.
-// Они должны использоваться таким образом:
+// // по имени (Ann, John, Pete)
+// users.sort((a, b) => a.name > b.name ? 1 : -1);
 
-// arr.filter(inBetween(3,6)) – выбирает только значения между 3 и 6 (включительно).
-// arr.filter(inArray([1,2,3])) – выбирает только элементы, совпадающие с одним из элементов массива
+// // по возрасту (Pete, Ann, John)
+// users.sort((a, b) => a.age > b.age ? 1 : -1);
+// Можем ли мы сделать его короче, скажем, вот таким?
+
+users.sort(byField('name'));
+users.sort(byField('age'));
+// То есть, чтобы вместо функции, мы просто писали byField(fieldName).
+
+// Напишите функцию byField, которая может быть использована для этого.
 
 
 
 
-function inBetween(a,b){
-  return lol=> a<=lol && lol<=b
+function byField(params) {
+  return((a, b) => a[params] > b[params] ? 1 : -1
 }
-function inArray(params) {
-  return lol => params.includes(lol)
-}
-
-
-let arr = [1, 2, 3, 4, 5, 6, 7];
-
-alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
-
-alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
