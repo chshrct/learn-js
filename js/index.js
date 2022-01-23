570be672bd72d8434855bca53b965e57ef9f6d20
Yauheni <eugene.kostroma@gmail.com>
2022-01-23 23:46:02 +0400

    function-prototype-task2

--- js/index.js
+++ js/index.js
@@ -1,60 +1,15 @@
-// В коде ниже мы создаём нового кролика new Rabbit, а потом пытаемся изменить его прототип.
+// Представьте, что у нас имеется некий объект obj, созданный функцией-конструктором – мы не знаем какой именно, но хотелось бы создать ещё один объект такого же типа.
 
-// Сначала у нас есть такой код:
+// Можем ли мы сделать так?
 
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
+let obj2 = new obj.constructor();
+// Приведите пример функции-конструктора для объекта obj, с которой такой вызов корректно сработает. И пример функции-конструктора, с которой такой код поведёт себя неправильно.
 
-let rabbit = new Rabbit();
+function User(name) {
+  this.name = name;
+}
 
-alert( rabbit.eats ); // true
-// Добавим одну строчку (выделенную в коде ниже). Что вызов alert покажет нам сейчас?
+let user = new User('John');
+let user2 = new user.constructor('Pete');
 
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
-
-let rabbit = new Rabbit();
-
-Rabbit.prototype = {};
-
-alert( rabbit.eats ); // true
-// …А если код такой (заменили одну строчку)?
-
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
-
-let rabbit = new Rabbit();
-
-Rabbit.prototype.eats = false;
-
-alert( rabbit.eats ); // false
-// Или такой (заменили одну строчку)?
-
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
-
-let rabbit = new Rabbit();
-
-delete rabbit.eats;
-
-alert( rabbit.eats ); // true
-// Или, наконец, такой:
-
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
-
-let rabbit = new Rabbit();
-
-delete Rabbit.prototype.eats;
-
-alert( rabbit.eats ); // undefined
+alert( user2.name );
