9484ba229d29e027c59a592e4bfdbd6b4a08dd56
Yauheni <eugene.kostroma@gmail.com>
2022-01-24 13:21:19 +0400

    prototype-methods-task2

--- js/index.js
+++ js/index.js
@@ -1,32 +1,16 @@
-// Имеется объект dictionary, созданный с помощью Object.create(null) для хранения любых пар ключ/значение.
+// Давайте создадим новый объект rabbit:
 
-// Добавьте ему метод dictionary.toString(), который должен возвращать список ключей, разделённых запятой. Ваш toString не должен выводиться при итерации объекта с помощью цикла for..in.
-
-// Вот так это должно работать:
-
-let dictionary = Object.create(null,{
-    toString:{
-        value(){
-            return Object.keys(this).join()
-        }
-    }
-    
-});
-
-// ваш код, который добавляет метод dictionary.toString
-
-
-
-
-
-// добавляем немного данных
-dictionary.apple = "Apple";
-dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
-
-// только apple и __proto__ выведены в цикле
-for(let key in dictionary) {
-  alert(key); // "apple", затем "__proto__"
+function Rabbit(name) {
+  this.name = name;
 }
+Rabbit.prototype.sayHi = function() {
+  alert(this.name);
+};
 
-// ваш метод toString в действии
-alert(dictionary); // "apple,__proto__"
+let rabbit = new Rabbit("Rabbit");
+// Все эти вызовы делают одно и тоже или нет?
+
+rabbit.sayHi(); //rabbit.name
+Rabbit.prototype.sayHi(); // alert undefined
+Object.getPrototypeOf(rabbit).sayHi();  //alert undefined
+rabbit.__proto__.sayHi(); // alert undefined
