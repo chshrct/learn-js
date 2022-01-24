903f714146601bfa0450753bf4b6940bc9b072ff
Yauheni <eugene.kostroma@gmail.com>
2022-01-24 13:12:41 +0400

    prototype-methods-task1

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
-// Добавьте всем функциям в прототип метод defer(ms), который возвращает обёртку, откладывающую вызов функции на ms миллисекунд.
+// Имеется объект dictionary, созданный с помощью Object.create(null) для хранения любых пар ключ/значение.
 
-// Например, должно работать так:
-Function.prototype.defer = function(ms){
-  let contf = this;
-  return function(...rest){
-      setTimeout(
-        ()=>contf.apply(this,rest),ms
-      )
-  }
-  }
+// Добавьте ему метод dictionary.toString(), который должен возвращать список ключей, разделённых запятой. Ваш toString не должен выводиться при итерации объекта с помощью цикла for..in.
+
+// Вот так это должно работать:
+
+let dictionary = Object.create(null,{
+    toString:{
+        value(){
+            return Object.keys(this).join()
+        }
+    }
+    
+});
+
+// ваш код, который добавляет метод dictionary.toString
 
 
-function f(a, b) {
-  alert( a + b );
+
+
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for(let key in dictionary) {
+  alert(key); // "apple", затем "__proto__"
 }
 
-f.defer(5000)(4, 5); // выведет 3 через 1 секунду.
-// Пожалуйста, заметьте, что аргументы должны корректно передаваться оригинальной функции.
-
+// ваш метод toString в действии
+alert(dictionary); // "apple,__proto__"
