699a7c492d99117f40cc5db9d5fde22fa39a7878
Yauheni <eugene.kostroma@gmail.com>
2022-01-25 13:46:46 +0400

    class-inheritance-taks2

--- js/index.js
+++ js/index.js
@@ -1,22 +1,54 @@
-// В коде ниже класс Rabbit наследует Animal.
+// У нас есть класс Clock. Сейчас он выводит время каждую секунду
 
-// К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
-
-class Animal {
-
-  constructor(name) {
-    this.name = name;
+class Clock {
+  constructor({ template }) {
+    this.template = template;
   }
 
-}
+  render() {
+    let date = new Date();
 
-class Rabbit extends Animal {
-  constructor(name) {
-    super(name);
-    this.name = name;
-    this.created = Date.now();
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
   }
 }
 
-let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
-alert(rabbit.name);
+class ExtendedClock extends Clock{
+    constructor({ template },precision){
+        super({template});
+        this.precision = 1000;
+        if(precision){
+            this.precision = precision;
+        }
+    }
+    start(){
+        this.render();
+        this.timer = setInterval(() => this.render(), this.precision);
+    }
+}
+
+let z = new ExtendedClock({template: 'h:m:s'});
+z.start();
+// Создайте новый класс ExtendedClock, который будет наследоваться от Clock и добавьте параметр precision – количество миллисекунд между «тиками». Установите значение в 1000 (1 секунда) по умолчанию.
