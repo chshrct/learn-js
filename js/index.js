d17a4efcafdb08ef14309f7648c4779862b78f5f
Yauheni <eugene.kostroma@gmail.com>
2022-01-25 13:29:50 +0400

    class-inheritance-taks1

--- js/index.js
+++ js/index.js
@@ -1,40 +1,22 @@
-class Clock{
-    
-    constructor({template}){
-        this.template=template
-    }
+// В коде ниже класс Rabbit наследует Animal.
 
-  
-    render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = this.template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    }
-  
-    stop() {
-      clearInterval(this.timer);
-    }
-  
-    start() {
-      this.render();
-      this.timer = setInterval(()=>this.render(), 1000);
-    }
-  
+// К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
   }
-  
-  let clock = new Clock({template: 'h:m:s'});
-  clock.start();
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+alert(rabbit.name);
