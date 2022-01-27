76a31b238cdef358041d2ff3738ba6f7d6d624b3
Yauheni <eugene.kostroma@gmail.com>
2022-01-27 21:48:48 +0400

    fcc-certification-task3

--- js/index.js
+++ js/index.js
@@ -1,54 +1,27 @@
-function convertToRoman(num) {
+function rot13(str) {
 
-    let romanian = '';
-    let numArr = num.toString().split('').map(
-      el=>+el
-    )
-  
-  function makeRomanianFromBasis(number,base){
-    // data
-    // matrix of romanian basis
-    let bas = [
-      ['I','V'], //1,5
-      ['X','L'], //10,50
-      ['C','D'], //100,500
-      ['M','V"'] //1000,5000
-    ];
-  
-    switch (number) {
-      case 0:
-        return ''
-      case 1:
-        return bas[base][0]
-      case 2:
-        return bas[base][0]+bas[base][0]
-      case 3:
-        return bas[base][0]+bas[base][0]+bas[base][0]
-      case 4:
-        return bas[base][0]+bas[base][1]
-      case 5:
-        return bas[base][1]
-      case 6:
-        return bas[base][1]+bas[base][0]
-      case 7:
-        return bas[base][1]+bas[base][0]+bas[base][0]
-      case 8:
-        return bas[base][1]+bas[base][0]+bas[base][0]+bas[base][0]
-      case 9:
-        return bas[base][0] + bas[base+1][0]
-  
-      default:
-        break;
+    let alphArr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let rot13 = makeRot13(alphArr);
+    
+    function makeRot13(alphArr){
+      let newArr = [...alphArr];
+      
+      for (let i =0;i<13;i++){
+        newArr.push(newArr.shift())
+      }
+      return newArr
     }
-  }
-  
-  
-  for(let i = 0; i<numArr.length;i++){
-  
-  romanian += makeRomanianFromBasis(numArr[i],numArr.length-i-1)
-  
-  };
-   return romanian;
-  }
-  
-  convertToRoman(36);
+    
+      return str.split('').map(
+        el=>{
+          if(alphArr.indexOf(el)>=0){
+            return rot13[alphArr.indexOf(el)]
+          }else{
+            return el
+          }      
+        }     
+      )
+      .join('')
+    }
+    
+    rot13("SERR PBQR PNZC");
