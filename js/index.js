a2e0254f667440b0aa3e10deed24be45c066ebb1
Yauheni <eugene.kostroma@gmail.com>
2022-01-27 21:26:37 +0400

    FCC-certification-task2

--- js/index.js
+++ js/index.js
@@ -1,87 +1,54 @@
-function checkCashRegister(price, cash, cid) {
-  //prepare data with everything *100 god bless JS
+function convertToRoman(num) {
 
-  let amountToPay = Math.round((cash - price) * 100);
-
-  let cidIntoObj = {};
-  cid.map((el) => {
-    cidIntoObj[el[0]] = Math.round(el[1] * 100);
-  });
-
-  let changeWeGive = [];
-  let tempmoneyData = ["ONE HUNDRED","TWENTY","TEN","FIVE","ONE","QUARTER","DIME","NICKEL","PENNY"];
-
-  let moneyData = {
-    "PENNY": 1,
-    "NICKEL": 5,
-    "DIME": 10,
-    "QUARTER": 25,
-    "ONE": 100,
-    "FIVE": 500,
-    "TEN": 1000,
-    "TWENTY": 2000,
-    "ONE HUNDRED": 10000,
-  };
-
-  function processOne(
-    amountToPay,
-    cidIntoObj,
-    changeWeGive,
-    moneyData,
-    whichMoney
-  ) {
-    if (amountToPay >= moneyData[whichMoney] && cidIntoObj[whichMoney]) {
-      if (amountToPay >= cidIntoObj[whichMoney]) {
-        amountToPay -= cidIntoObj[whichMoney];
-        changeWeGive.push([whichMoney, cidIntoObj[whichMoney] / 100]);
-        cidIntoObj[whichMoney] = 0;
-      } else {
-        const howMuchInHundreds =
-          Math.floor(amountToPay / moneyData[whichMoney]) *
-          moneyData[whichMoney];
-        amountToPay -= howMuchInHundreds;
-        changeWeGive.push([whichMoney, howMuchInHundreds / 100]);
-        cidIntoObj[whichMoney] -= howMuchInHundreds;
-      }
-    }
-
-    return [amountToPay, cidIntoObj, changeWeGive];
-  }
-
-  tempmoneyData.forEach(
-    el=>[amountToPay, cidIntoObj, changeWeGive] = processOne(
-      amountToPay,
-      cidIntoObj,
-      changeWeGive,
-      moneyData,
-      el
-    )
+  let romanian = '';
+  let numArr = num.toString().split('').map(
+    el=>+el
   )
 
- 
-  if (amountToPay > 0) {
-    return { status: "INSUFFICIENT_FUNDS", change: [] };
+function makeRomanianFromBasis(number,base){
+  // data
+  // matrix of romanian basis
+  let bas = [
+    ['I','V'], //1,5
+    ['X','L'], //10,50
+    ['C','D'], //100,500
+    ['M','V"'] //1000,5000
+  ];
+
+  switch (number) {
+    case 0:
+      return ''
+    case 1:
+      return bas[base][0]
+    case 2:
+      return bas[base][0]+bas[base][0]
+    case 3:
+      return bas[base][0]+bas[base][0]+bas[base][0]
+    case 4:
+      return bas[base][0]+bas[base][1]
+    case 5:
+      return bas[base][1]
+    case 6:
+      return bas[base][1]+bas[base][0]
+    case 7:
+      return bas[base][1]+bas[base][0]+bas[base][0]
+    case 8:
+      return bas[base][1]+bas[base][0]+bas[base][0]+bas[base][0]
+    case 9:
+      return bas[base][0] + bas[base+1][0]
+
+    default:
+      break;
   }
-
-  let whatsleft = Object.values(cidIntoObj).reduce((sum, el) => sum + el, 0);
-
-  if (whatsleft > 0) {
-    return { status:"OPEN", change: changeWeGive };
-  }
-
-  return { status: "CLOSED", change: cid };
 }
 
-console.log(
-  checkCashRegister(3.26, 100, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100],
-  ])
-);
+
+for(let i = 0; i<numArr.length;i++){
+
+romanian += makeRomanianFromBasis(numArr[i],numArr.length-i-1)
+
+};
+ return romanian;
+}
+
+convertToRoman(36);
