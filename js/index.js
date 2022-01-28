f26945df26bcf11296af9e3f793c7e70d5d6e252
Yauheni <eugene.kostroma@gmail.com>
2022-01-28 23:36:44 +0400

    dom-navigation-task3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,1 @@
-// Если elem – произвольный узел DOM-элемента…
-
-// Правда, что elem.lastChild.nextSibling всегда равен null? yes
-// Правда, что elem.children[0].previousSibling всегда равен null ? no
+// script in html
