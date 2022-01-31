e8577a6de938e5c6600a30211cb58a21e0fba1c5
Yauheni <eugene.kostroma@gmail.com>
2022-01-31 17:51:00 +0400

    codewars-MaketheDeadfishSwim

--- js/index.js
+++ js/index.js
@@ -1,30 +1,9 @@
-function solution(roman){
-    // complete the solution by transforming the 
-    // string roman numeral into an integer  
-    
-    const romanValue = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000,
-    };
+// Return the output array, and ignore all non-op characters
+function parse( data )
+{
+  let val  = 0;
+  const dataArr = data.split('');
+  console.log(dataArr);
+}
 
-    let result = roman.split('').reduce(
-        (sum,el,index)=>{
-            if(romanValue[el]>=romanValue[roman.split('')[index+1]] || !(romanValue[roman.split('')[index+1]])){
-                return sum+romanValue[el]
-            }else{
-                return sum-romanValue[el]
-            }
-        },0
-    )
-    
-    return result
-    
-    
-  }
-
-solution('XXI'); 
+parse("iiisdoso");
