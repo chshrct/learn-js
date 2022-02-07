3ecf8badb9c3c4f82174728d8e258a79eeb45ac8
Yauheni <eugene.kostroma@gmail.com>
2022-02-07 14:35:58 +0400

    codewars-int32toIPv4

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-const linkCollectiion = document.querySelectorAll('a');
+function int32ToIp(int32){
 
-for(link of linkCollectiion){
-    if(link.getAttribute('href')!==null &&link.getAttribute('href').includes('://') && !(link.href.includes('http://internal.com'))){
-        link.style.color = 'orange'
-    }
+  let bin = int32.toString(2)
+
+for (i=0;i<32-int32.toString(2).length;i++){
+    bin='0'+bin
 }
+    return `"${parseInt(bin.slice(0,8),2)}.${parseInt(bin.slice(8,16),2)}.${parseInt(bin.slice(16,24),2)}.${parseInt(bin.slice(24,32),2)}"`
+}
+
+int32ToIp(2149583361)
