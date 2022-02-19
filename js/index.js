6de386d0be325c28bbc5b81068b70a949a3e92b8
Yauheni <eugene.kostroma@gmail.com>
2022-02-19 22:53:47 +0400

    modifying-document-task5

--- js/index.js
+++ js/index.js
@@ -1,11 +1,40 @@
-let a;
-do{
+// Напишите функцию createTree, которая создаёт вложенный список ul/li из объекта.
+//
+//     Например:
 
-        a=prompt('Введи дело')
-        console.log(a)
-    if(a){
-        let z = document.createElement("li")
-        z.textContent=a
-        document.getElementById('list').append(z)
+let data = {
+    "Рыбы": {
+        "форель": {},
+        "лосось": {}
+    },
+
+    "Деревья": {
+        "Огромные": {
+            "секвойя": {},
+            "дуб": {}
+        },
+        "Цветковые": {
+            "яблоня": {},
+            "магнолия": {}
+        }
     }
-    }while(a)
+}
+// Синтаксис:
+
+let container = document.getElementById('container');
+
+const createTree = (cont, obj) => {
+
+    const unorderedList = document.createElement('ul')
+    cont.append(unorderedList)
+
+    for (let key in obj) {
+        let listItem = document.createElement('li')
+        listItem.textContent = key
+        unorderedList.append(listItem)
+        Object.keys(obj[key]).length>0 && createTree(listItem,obj[key])
+    }
+
+}
+
+createTree(container, data); // создаёт дерево в контейнере
