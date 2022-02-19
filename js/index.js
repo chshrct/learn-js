d6160fd0d178ec57348f8cef92f64781665af8c3
Yauheni <eugene.kostroma@gmail.com>
2022-02-19 23:50:45 +0400

    modifying-document-task7

--- js/index.js
+++ js/index.js
@@ -1,9 +1,45 @@
-const listItems =  document.querySelectorAll('li')
-console.log(listItems)
-for(let li of listItems){
-    const count = li.querySelectorAll('li').length
-    const text = document.createTextNode(`[${count}]`)
+function createCalendar(elem, year, month) {
 
-    count>0 && (li.firstChild.after(text))
+    let mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
+    let d = new Date(year, mon);
 
+    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+    // пробелы для первого ряда
+    // с понедельника до первого дня месяца
+    // * * * 1  2  3  4
+    for (let i = 0; i < getDay(d); i++) {
+        table += '<td></td>';
+    }
+
+    // <td> ячейки календаря с датами
+    while (d.getMonth() == mon) {
+        table += '<td>' + d.getDate() + '</td>';
+
+        if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
+            table += '</tr><tr>';
+        }
+
+        d.setDate(d.getDate() + 1);
+    }
+
+    // добить таблицу пустыми ячейками, если нужно
+    // 29 30 31 * * * *
+    if (getDay(d) != 0) {
+        for (let i = getDay(d); i < 7; i++) {
+            table += '<td></td>';
+        }
+    }
+
+    // закрыть таблицу
+    table += '</tr></table>';
+
+    elem.innerHTML = table;
 }
+
+function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+    let day = date.getDay();
+    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+    return day - 1;
+}
+createCalendar(calendar, 2012, 9);
