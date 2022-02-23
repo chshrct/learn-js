cf4b07905c81ea25177cd58f24abe8ba8e247fcb
Yauheni <eugene.kostroma@gmail.com>
2022-02-23 23:55:46 +0400

    size-and-scroll-task2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-let scrollBottom = elem.scrollHeight - elem.scrollTop - elem.clientHeight;
+let div = document.createElement('div');
+
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+
+// мы должны вставить элемент в документ, иначе размеры будут равны 0
+document.body.append(div);
+let scrollWidth = div.offsetWidth - div.clientWidth;
+
+div.remove();
+
+alert(scrollWidth);
