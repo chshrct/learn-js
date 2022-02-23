999189f62fe86a83d3c9bc192e3910a59a24434c
Yauheni <eugene.kostroma@gmail.com>
2022-02-23 16:56:36 +0400

    modifying-document-task11

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
+const names = document.querySelectorAll('td:first-child')
+const namesArr = Array.from(names)
+
+namesArr.sort((a, b) =>a.innerHTML.localeCompare(b.innerHTML))
+let randomArr = []
+namesArr.forEach(e=>randomArr.push(e.innerHTML))
+let i = 0;
+for (let name of names){
+    name.innerHTML = randomArr[i]
+    i++
+}
