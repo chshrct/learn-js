b60bc6d2e71a4b516f298b7a4f6cde5ac3050fa1
Yauheni <eugene.kostroma@gmail.com>
2022-02-23 15:32:46 +0400

    modifying-document-task8

--- js/index.js
+++ js/index.js
@@ -1,41 +1,54 @@
-function createCalendar(elem, year, month) {
+const elem = document.getElementById('elem')
 
-    let mon = month - 1;
-    let d = new Date(year, mon);
+function createCalendar(elem, year, month){
 
-    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
-
-
-    for (let i = 0; i < getDay(d); i++) {
-        table += '<td></td>';
-    }
-
-
-    while (d.getMonth() == mon) {
-        table += '<td>' + d.getDate() + '</td>';
-
-        if (getDay(d) % 7 == 6) {
-            table += '</tr><tr>';
+    const firstDayOfMonth = new Date(year,month-1)
+    const lastDayOfMonth = new Date(year,month,0)
+    console.log(lastDayOfMonth.getDate())
+    const rowsCount = Math.ceil(lastDayOfMonth.getDate()/7)
+    const table = document.createElement('table')
+    const arrayOfDates = new Array(rowsCount*7)
+    for (let i = 0; i < arrayOfDates.length-1; i++) {
+        if (i===firstDayOfMonth.getDay()-1){
+            for (let j = 0; j < lastDayOfMonth.getDate(); j++) {
+                arrayOfDates[i+j]=j+1
+            }
         }
 
-        d.setDate(d.getDate() + 1);
     }
+    console.log(arrayOfDates)
 
+    table.innerHTML = '<tr>\n' +
+        '    <th>пн</th>\n' +
+        '    <th>вт</th>\n' +
+        '    <th>ср</th>\n' +
+        '    <th>чт</th>\n' +
+        '    <th>пт</th>\n' +
+        '    <th>сб</th>\n' +
+        '    <th>вс</th></tr>'
 
-    if (getDay(d) != 0) {
-        for (let i = getDay(d); i < 7; i++) {
-            table += '<td></td>';
+    for (let i = 0; i < rowsCount*7; i++) {
+        if (!(i%7)){
+            const row = document.createElement('tr')
+            for (let j = 0; j < 7; j++) {
+                const day = document.createElement('td')
+                if(arrayOfDates[0]){
+                    day.innerHTML=arrayOfDates.shift().toString()
+                }else{
+                    arrayOfDates.shift()
+                }
+
+                row.append(day)
+            }
+            table.firstElementChild.append(row)
         }
+
+
     }
 
-    table += '</tr></table>';
+    elem.prepend(table)
 
-    elem.innerHTML = table;
 }
 
-function getDay(date) {
-    let day = date.getDay();
-    if (day == 0) day = 7;
-    return day - 1;
-}
-createCalendar(calendar, 2012, 9);
+
+createCalendar(elem,2012,10)
