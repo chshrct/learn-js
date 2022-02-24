d933f37a3f5348d5306ed26d268ccbd3c33feb2b
Yauheni <eugene.kostroma@gmail.com>
2022-02-24 23:29:33 +0400

    coordinates-task2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,45 @@
-const div = document.getElementById('field')
-const divRect = div.getBoundingClientRect()
-console.log(divRect.x,divRect.y)
-console.log(divRect.right,divRect.bottom)
+/**
+ * Позиционирует элемент elem относительно элемента anchor в соответствии со значением position.
+ *
+ * @param {Node} anchor     элемент, около которого позиционируется другой элемент
+ * @param {string} position одно из: top/right/bottom
+ * @param {Node} elem       элемент, который позиционируется
+ *
+ * Оба элемента elem и anchor должны присутствовать в документе
+ */
+function positionAt(anchor, position, elem) {
+    const anchorRect = anchor.getBoundingClientRect()
+    const elemHeight = elem.offsetHeight
+    if (position ==='top'){
+        elem.style.left = anchorRect.left + 'px'
+        elem.style.top = (anchorRect.top-elemHeight) + 'px'
+    }
+    if (position ==='right'){
+        elem.style.left = anchorRect.right + 'px'
+        elem.style.top = anchorRect.top + 'px'
+    }
+    if (position ==='bottom'){
+        elem.style.left = anchorRect.left + 'px'
+        elem.style.top = anchorRect.bottom + 'px'
+    }
+}
 
-console.log(divRect.x+div.clientLeft,divRect.y+div.clientTop)
-console.log(divRect.x+div.clientLeft+div.clientWidth,divRect.y+div.clientTop+div.clientHeight)
+/**
+ * Показывает заметку с заданным содержимым на заданной позиции
+ * относительно элемента anchor.
+ */
+function showNote(anchor, position, html) {
+    let note = document.createElement('div');
+    note.className = "note";
+    note.innerHTML = html;
+    document.body.append(note);
+
+    positionAt(anchor, position, note);
+}
+
+// test it
+let blockquote = document.querySelector('blockquote');
+
+showNote(blockquote, "top", "note above");
+showNote(blockquote, "right", "note at the right");
+showNote(blockquote, "bottom", "note below");
