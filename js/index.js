f68577daff5951bd61296229aa6d4d7504d7c91c
Yauheni <eugene.kostroma@gmail.com>
2022-02-25 12:52:28 +0400

    introduction-browser-events-task3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-const button = document.querySelector('#hider')
+// В переменной button находится кнопка. Изначально на ней нет обработчиков.
+//
+//     Который из обработчиков запустится? Что будет выведено при клике после выполнения кода?
 
-button.addEventListener('click',()=>button.hidden=true)
+    button.addEventListener("click", () => alert("1"));
+
+button.removeEventListener("click", () => alert("1"));
+
+button.onclick = () => alert(2);
+
+// 1,2
