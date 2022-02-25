96bfb497ca3ec80000ca9710ba6c328cff84b030
Yauheni <eugene.kostroma@gmail.com>
2022-02-25 14:20:37 +0400

    introduction-browser-events-task5

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
-function clickHandler(e) {
-    const fieldCoords = field.getBoundingClientRect()
-    let left = e.clientX - fieldCoords.left - field.clientLeft - ball.offsetWidth / 2
-    let top = e.clientY - fieldCoords.top - field.clientTop - ball.offsetHeight / 2
+const open = document.getElementById('open')
+const closed = document.getElementById('closed')
+const list = document.getElementById('list')
+const toggle = document.getElementById('toggle')
 
-
-    if (top < 0) top = 0;
-    if (left < 0) left = 0;
-    if (left + ball.clientWidth > field.clientWidth) {
-        left = field.clientWidth - ball.clientWidth;
-    }
-    if (top + ball.clientHeight > field.clientHeight) {
-        top = field.clientHeight - ball.clientHeight;
-    }
-
-    ball.style.left = left + 'px'
-    ball.style.top = top + 'px'
+function toggleHandler(){
+    open.classList.toggle('hidden')
+    closed.classList.toggle('hidden')
+    list.classList.toggle('hidden')
 }
 
-const ball = document.getElementById('ball')
-const field = document.getElementById('field')
-field.addEventListener('click', clickHandler)
+toggle.addEventListener('click',toggleHandler)
+
