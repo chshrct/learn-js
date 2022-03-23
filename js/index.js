3e55620ee64191149d51d93a39e1b272fbfa5a2c
Yauheni <eugene.kostroma@gmail.com>
2022-03-23 22:46:44 +0400

    default-browser-action-task1

--- js/index.js
+++ js/index.js
@@ -1,28 +1,17 @@
-const addTooltip = (e) => {
-    if (typeof (e.target.dataset.tooltip) !== 'string') return
-    const button = e.target
-    const tooltip = document.createElement('div');
-    tooltip.className = 'tooltip';
-    tooltip.textContent = e.target.dataset.tooltip;
-    document.body.prepend(tooltip);
+const contents = document.querySelector('#contents')
 
-    const buttonClientRect = button.getBoundingClientRect();
-    const tooltipClientRect = tooltip.getBoundingClientRect();
+const linksListener = (
+    /** @type {Event} **/
+    event) => {
+    event.preventDefault()
 
+    const target = event.target
+    const link = target.closest('a')
+    if (link && contents.contains(link)) {
+        confirm(`Leave for ${link.href}`)
+            && (window.location = link.href)
+    }
 
-    (buttonClientRect.top < tooltipClientRect.height + 5)
-        ? tooltip.style.top = `${buttonClientRect.bottom + 5}px`
-        : tooltip.style.top = `${buttonClientRect.top - tooltipClientRect.height - 5}px`;
-
-    (buttonClientRect.width / 2 + buttonClientRect.left > tooltipClientRect.width / 2)
-        ? tooltip.style.left = `${(buttonClientRect.width / 2 + buttonClientRect.left) - (tooltipClientRect.width / 2)}px`
-        : tooltip.style.left = `${buttonClientRect.left}px`;
-}
-const removeTooltip = (event) => {
-    if (typeof (event.target.dataset.tooltip) !== 'string') return
-    document.querySelector('.tooltip').remove()
 }
 
-
-document.addEventListener('mouseover', addTooltip)
-document.addEventListener('mouseout', removeTooltip)
+contents.addEventListener('click', linksListener)
