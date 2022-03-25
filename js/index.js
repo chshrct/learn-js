f8caeaeb1e0dc500d9183ac077d1e94233ac8191
Yauheni <eugene.kostroma@gmail.com>
2022-03-25 17:36:37 +0400

    onscroll-task1

--- js/index.js
+++ js/index.js
@@ -1,33 +1,11 @@
-function runOnKeys(func,...args){
-
-    let pressedKeys = new Set()
-
-    const comparePressedWithArgs = (pressed,args) => {
-        for (let key of pressed) {
-            if (!args.includes(key)) return false      
-        }
-        return true
+function populate() {
+    while(true) {
+      let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+      if (windowRelativeBottom > document.documentElement.clientHeight + 100) break;
+      document.body.insertAdjacentHTML("beforeend", `<p>Date: ${new Date()}</p>`);
     }
+  }
 
-    const keyDown = function(/** @type {KeyboardEvent} **/event){
+  window.addEventListener('scroll', populate);
 
-        pressedKeys.add(event.code)
-        if (pressedKeys.size<2 ||  !comparePressedWithArgs(pressedKeys,args) ) return
-        pressedKeys.clear()
-        return func()
-    }
-
-    const keyUp = function(/** @type {KeyboardEvent} **/event){
-        pressedKeys.delete(event.code)
-    }
-
-    document.addEventListener('keydown',keyDown)
-    document.addEventListener('keyup',keyUp)
-
-}
-
-runOnKeys(
-    () => alert("Привет!"),
-    "KeyQ",
-    "KeyW"
-  );
+  populate();
